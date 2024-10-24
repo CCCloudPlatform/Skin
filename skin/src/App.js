ae1f3652e610d8a13a07f3470d3d3425f9171ae4
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2024-10-24 18:32:43 +0900

    feat : Service-Overview Page with Service Card Components (#6)

--- skin/src/App.js
+++ skin/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
+import ServiceOverview from './pages/ServiceOverview';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/service-overview" element={<ServiceOverview />} />
             {/* Add more routes here as needed */}
           </Routes>
         </div>
